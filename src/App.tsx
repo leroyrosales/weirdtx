eb9e603e1f863e4bcbea325b1026aa8b2cdaa756
Leroy Rosales <leroyrosales@gmail.com>
2023-02-08 16:42:39 -0600

    Removes unused var

--- src/App.tsx
+++ src/App.tsx
@@ -24,23 +24,6 @@ function App() {
     )
   }
 
-  let subtitle;
-  const [modalIsOpen, setIsOpen] = useState(false);
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00';
-  }
-
-  function closeModal() {
-    setIsOpen(false);
-  }
-
-
   return (
     <>
       <div className="maps_border">
@@ -51,12 +34,6 @@ function App() {
               <Marker
                 key={park.location.location_id}
                 position={[park.geometry.coordinates[0], park.geometry.coordinates[1]]}
-
-                eventHandlers={{
-                  click: () => {
-                    openModal
-                  },
-                }}
               >
                 <Popup
                   position={[park.geometry.coordinates[0], park.geometry.coordinates[1]]}
